1ada9117120141ac24950cd8839a2ef909326108
Melati Anggraini <112107340+mlatia@users.noreply.github.com>
2024-12-01 09:38:52 +0700

    add link streaming

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -9,6 +9,11 @@ export default function About() {
     <div className='w-full'>
       <BannerAbout></BannerAbout>
       <Trailer></Trailer>
+      <div className='flex justify-center items-center py-4 gap-4'>
+        <div className='border-2 bg-[#FADB44] border-black md:h-10 md:w-10 h-6 w-6 rounded-full'></div>
+        <a href="https://youtube.com/live/x8dZZWONKtU" target="_blank" rel="noopener noreferrer"><h2 className='text-primary md:text-5xl text-xl hover:underline hover:underline-offset-2 cursor-pointer'>Live Streaming Youtube!</h2></a>
+        <div className='border-2 bg-[#FADB44] border-black md:h-10 md:w-10 h-6 w-6 rounded-full'></div>
+      </div>
       <OverviewAbout></OverviewAbout>
       <Rca></Rca>
       <div className='flex justify-center items-center py-4 gap-4'>
